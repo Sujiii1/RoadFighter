3ba77ac6fe6b78c19ae9fd4868296b5b7a036093
Sujiii1 <tnwlsqp12@gmail.com>
2024-06-05 22:04:02 +0900

    [Feat] LoadLoop

--- Assets/3.Script/RoadLoop.cs
+++ Assets/3.Script/RoadLoop.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class RoadLoop : MonoBehaviour
 {
-    private Vector3 startPos;
-    private float repeatWidth;
 
-    private void Awake()
-    {
-        startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider>().size.z / 2;
-    }
+    [SerializeField] private List<Transform> roadSegments;
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float resetPositionZ = -10f;
+    [SerializeField] private float roadSegmentLength = 10f;  //도로 조각 길이
+
 
     private void Update()
     {
-        if (transform.position.z < startPos.z - repeatWidth)
+        RepeatRoad();
+    }
+
+    private void RepeatRoad()
+    {
+        for(int i= 0; i<roadSegments.Count; i++)
         {
-            transform.position = startPos;
+            roadSegments[i].Translate(Vector3.back * speed * Time.deltaTime);   //도로 z축 이동
+
+            //resetPositionZ 에서 맨 뒤로 이동
+            if(roadSegments[i].position.z <= resetPositionZ)
+            {
+                Vector3 newPos = roadSegments[GetLastIndex()].position;
+                newPos.z += roadSegmentLength;
+
+                //마지막 도로 맨 뒤로 이동
+                roadSegments[i].position = newPos;
+
+                Transform temp = roadSegments[i];
+                roadSegments.RemoveAt(i);
+                roadSegments.Add(temp);     //List에서 제거 후 맨 뒤에 추가-> 순서 유지
+
+                i--; 
+            }
         }
+    }
+
+    private int GetLastIndex()
+    {
+        return roadSegments.Count - 1;  //마지막 도로의 인덱스 반환
     }
 }
